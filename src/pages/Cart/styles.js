fefd18f9620328c311a5e3cdb40ed9db6fd79d76
Jonas Vargaski <jonasvargaski@hotmail.com>
2020-01-16 23:22:14 -0300

    first commit

--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 1000px;
+  width: 100%;
+  margin: 20px auto;
+  padding: 0 10px;
+`;
